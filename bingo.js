72fa20f1d8f58a14bc6325d4fb84a5db2b48e5f1
luis-16 <58233197+luis-16@users.noreply.github.com>
2020-12-26 16:35:12 -0500

    Update bingo.js

--- bingo.js
+++ bingo.js
@@ -1,18 +1,14 @@
 function tocar(numero) {
-    if (document.getElementById(numero) != "black" ) {
-        //document.getElementById(numero).style.background = "black";
-        //document.getElementById(numero).style.color = "white";
-    } else {
-        //document.getElementById(numero).style.background = "red";
-        //document.getElementById(numero).style.color = "";
-    }
-    //console.log(document.getElementById(numero).classList,document.getElementById(numero).classList.value,document.getElementById(numero).classList.value.split(" ").indexOf('seleccionada'));
-
     if (document.getElementById(numero).classList.value.split(" ").indexOf('seleccionada') == -1) {
         document.getElementById(numero).classList.add('seleccionada');        
     } else {
         document.getElementById(numero).classList.remove('seleccionada');        
     }
-    
+}
 
+function limpiar() {
+    document.querySelectorAll(".seleccionada").forEach(item => {
+        console.log(item);
+        item.classList.remove('seleccionada'); 
+    });
 }
